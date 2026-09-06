e02291f87ee39cb5dc7b4df848e16cf91e1778cf
Rakesh <rakeshdev9863@gmail.com>
2026-09-06 18:13:11 +0530

    Update tech stack for portfolio projects

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -147,7 +147,7 @@ export const portfolioData = {
         title: "Grabit – MERN Stack E-Commerce Website",
         description:
           "A full-stack e-commerce website for vegetables and home essentials.",
-        tech: ["React.js"],
+        tech: ["React.js","Node.js","Express.js","MongoDB","REST API"],
         image: "/images/Screenshot 2026-09-06 174409.png",
         live: "https://grabit-mern-stack.vercel.app/",
         github: "https://github.com/RakeshDev2003/Grabit-mern-stack",
@@ -157,7 +157,7 @@ export const portfolioData = {
         title: "E-commerce website ",
         description:
           "A scalable vegetalbe and home items website ",
-        tech: ["React.js","Node.js","Express.js","MongoDB","REST API" ],
+        tech: ["React.js"],
         image: "/images/Screenshot 2026-04-27 221141.png",
         live: "https://borobazar-by-rakesh.vercel.app/",
         github: "https://github.com/RakeshDev2003/BoroBazar-by-Rakesh",
